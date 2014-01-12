702e5729335bd7addee8aab069d5d923402cb22b
lindsayhenwood <lindsayhenwood@gmail.com>
2014-01-12 01:14:43 +0000

    added js function to make photos grow

--- photogallery.js
+++ photogallery.js
@@ -1,6 +1,20 @@
-$(document).ready(function()){
-	alert("ready!");
+$(document).ready(function(){
+
+	/*$('.li').mouseover(function){
+		$(this).stop().animate({"width": "100%", "left": "0px", "top": "0px"}, 400, 'swing'
+	}).mouseout(function(){
+		$(this).stop().animate({"width": "80%", "left": "15", "top": "15px"}, 200, 'swing'
+				});;
+			});
+		}
+	});*/
+
+	$("#portfolio-feed img").hover( function() {
+		$(this).animate({width: "250px", height: "225px", margin: "7.5px"}, 1000);
+	},  function() {
+		$(this).animate({width:"225px", height: "200px", margin: "20px"}, 1000);
+	});
+
+	/*$('.carousel').carousel({ interval: 2000})*/
+
 });
-
-
-$('.carousel').carousel()
